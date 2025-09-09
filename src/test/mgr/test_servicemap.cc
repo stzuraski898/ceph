54e1d072c1b92b16084c8b05eac87bbf749c4ef2
stzuraski898 <steven.zuraski@ibm.com>
2025-09-09 21:49:24 +0000

    test: added ServiceMap unit tests
    Signed-off-by: stzuraski898 <steven.zuraski@ibm.com>

--- src/test/mgr/test_servicemap.cc
+++ src/test/mgr/test_servicemap.cc
@@ -22,7 +22,7 @@ TEST(ServiceDaemon, Dump)
 
     test_daemon.gid = 11;
     addr.parse("127.0.0.1:1234", nullptr);
-    test_daemon.addr = (addr);  
+    test_daemon.addr = addr;  
     test_daemon.start_epoch = 33;
     test_daemon.start_stamp = utime_t(55, 5500000);
 
@@ -58,9 +58,11 @@ TEST(ServiceMapService, GetTaskSummary)
 {
     std::string expected = "\n    task:\n        svc.test: running\n        svc.test2: idle";
     ServiceMap::Service sms;
+    std::string summary;
+
     sms.daemons["test"].task_status["task"] = "running";
     sms.daemons["test2"].task_status["task"] = "idle";
-    std::string summary = sms.get_task_summary("svc");
+    summary = sms.get_task_summary("svc");
 
     EXPECT_EQ(summary, expected);
 }
@@ -69,11 +71,12 @@ TEST(ServiceMapService, CountMetadata)
 {
     ServiceMap::Service sms;
     std::map<std::string, int> metadata_info;
+
     sms.daemons["daemon1"].metadata["test"] = "t1";
     sms.daemons["daemon2"].metadata["test"] = "t2";
     sms.daemons["daemon3"].task_status["abc"] = "xyz";
-    
     sms.count_metadata("test", &metadata_info);
+
     EXPECT_EQ(metadata_info["t1"], 1);
     EXPECT_EQ(metadata_info["t2"], 1);
     EXPECT_EQ(metadata_info["unknown"], 1);
@@ -84,14 +87,34 @@ TEST(ServiceMapService, Dump)
     ServiceMap::Service sms;
     std::ostringstream oss;
     ceph::JSONFormatter f;
+    entity_addr_t addr;
     std::string json_str;
-    
-    sms.summary "testSummary";
-    s.daemons["d123"].gid = 123;
+    std::string expected = R"({"daemons":{"summary":"testSummary","d123":{"start_epoch":0,"start_stamp":"0.000000","gid":123,"addr":"127.0.0.1:1234/0","metadata":{},"task_status":{}}}})";
 
-    f.dump_object("service", sms);\
+    addr.parse("127.0.0.1:1234", nullptr);
+    sms.summary = "testSummary";
+    sms.daemons["d123"].gid = 123;
+    sms.daemons["d123"].addr = addr;
+    f.dump_object("service", sms);
     f.flush(oss);
     json_str = oss.str();
+
+    EXPECT_EQ(json_str, expected);
 }
 
+TEST(ServiceMapMap, Dump)
+{
+    std::ostringstream oss;
+    ceph::JSONFormatter f;
+    std::string json_str;
+    ServiceMap sm;
+    std::string expected = R"({"epoch":123,"modified":"0.000000","services":{"svc":{"daemons":{"summary":"testSummary"}}}})";
 
+    sm.epoch = 123;
+    sm.services["svc"].summary = "testSummary";
+    f.dump_object("servicemap", sm);
+    f.flush(oss);
+    json_str = oss.str();
+
+    EXPECT_EQ(json_str, expected);
+}
